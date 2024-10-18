dfa58b1fb7e5d8189a61f2745b2fd9b3a72a251d
Reiketsu77 <kantediery@gmail.com>
2024-10-18 20:10:41 +0200

    Create login.js

---
+++ FrontEnd/javascript/login.js
@@ -0,0 +1,6 @@
+const connection = document.getElementById('connection');
+
+connection.addEventListener('click', function() {
+    console.log('Le bouton "Se connecter" a été cliqué.');
+    window.location.href = 'index.html';
+});
