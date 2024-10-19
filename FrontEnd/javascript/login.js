91f88223483107feedc8616ca8fde5a2d099ce75
Reiketsu77 <kantediery@gmail.com>
2024-10-19 03:51:21 +0200

    Update login.js

--- FrontEnd/javascript/login.js
+++ FrontEnd/javascript/login.js
@@ -1,6 +1,11 @@
+const loginApiURL = 'http://localhost:5678/api/users/login';
 const connection = document.getElementById('connection');
 
-connection.addEventListener('click', function() {
+
+connection.addEventListener('click', function(event) {
     console.log('Le bouton "Se connecter" a été cliqué.');
+
+    console.log
+
     window.location.href = 'index.html';
 });
